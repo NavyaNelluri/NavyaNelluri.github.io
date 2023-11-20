79f1c682400197c39858e234b604b820dedf0e1e
NavyaNelluri <123142678+NavyaNelluri@users.noreply.github.com>
2023-11-20 10:37:55 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,92 @@
+window.onload = function () {
+    const themeToggle = document.getElementById('toggle-theme');
+    themeToggle.addEventListener('click', toggleTheme);
+
+    // Check for saved theme preference
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-theme');
+    }
+
+    // Apply slide-in animation on load
+    triggerSlideInAnimation();
+
+    // Add these lines to apply the active class and start info animation
+    const profileInfo = document.querySelector('.profile-info');
+    profileInfo.classList.add('active');
+
+    // Listen for the end of the profile pic transition
+    const profilePic = document.getElementById('profile-pic');
+    profilePic.addEventListener('transitionend', function () {
+        // Trigger the animation for profile info after the profile pic transition is complete
+        profileInfo.classList.add('info-slide-in');
+    });
+
+    // Trigger the animation for profile pic and info on every refresh
+    triggerSlideInAnimation();
+    profileInfo.classList.add('info-slide-in');
+};
+
+// Function to trigger slide-in animation
+function triggerSlideInAnimation() {
+    const profilePic = document.getElementById('profile-pic');
+    profilePic.classList.add('slide-in');
+}
+
+function toggleTheme() {
+    document.body.classList.toggle('dark-theme');
+    // Save the theme preference to local storage
+    const theme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+    localStorage.setItem('theme', theme);
+
+    // Adjust image style based on theme with animation
+    adjustImageStyleWithAnimation();
+}
+
+function adjustImageStyle() {
+    const profilePic = document.getElementById('profile-pic');
+
+    // Check if the theme is dark, and adjust image style accordingly
+    if (document.body.classList.contains('dark-theme')) {
+        profilePic.style.borderColor = '#fff'; // White border for dark theme
+    } else {
+        profilePic.style.borderColor = '#333'; // Dark border for light theme
+    }
+}
+
+function adjustImageStyleWithAnimation() {
+    const profilePic = document.getElementById('profile-pic');
+
+    // Add a class for the transition effect
+    profilePic.classList.add('border-transition');
+
+    // Adjust image style based on theme
+    adjustImageStyle();
+
+    // Remove the class after the transition is complete
+    setTimeout(() => {
+        profilePic.classList.remove('border-transition');
+    }, 500); // Adjust the duration to match the CSS transition duration
+}
+//education details toggle
+function toggleDetails(elementId) {
+        const details = document.getElementById(elementId);
+        details.classList.toggle('show-details');
+    }
+	
+function submitForm() {
+    // Get form data
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    // You can perform further actions with the form data, such as validation or sending it to a server.
+
+    // For now, let's log the data to the console
+    console.log('Form Data:', { name, email, message });
+
+    // Optionally, you can clear the form fields after submission
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+}
+
