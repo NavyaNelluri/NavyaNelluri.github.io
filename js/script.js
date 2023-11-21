1ce4ca18b9f0543819516e197549ec392b71cac2
NavyaNelluri <123142678+NavyaNelluri@users.noreply.github.com>
2023-11-21 01:18:54 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,61 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const themeToggle = document.getElementById('toggle-theme');
-    themeToggle.addEventListener('click', toggleTheme);
-
-    // Check for saved theme preference
-    if (localStorage.getItem('theme') === 'dark') {
-        document.body.classList.add('dark-theme');
-    }
-
-    // Apply slide-in animation on load
-    triggerSlideInAnimation();
-
-    // Add these lines to apply the active class and start info animation
-    const profileInfo = document.querySelector('.profile-info');
-    profileInfo.classList.add('active');
-
-    // Listen for the end of the profile pic transition
-    const profilePic = document.getElementById('profile-pic');
-
-    if (profilePic) {
-        profilePic.addEventListener('transitionend', function () {
-            // Trigger the animation for profile info after the profile pic transition is complete
-            profileInfo.classList.add('info-slide-in');
-        });
-    }
-
-    // Trigger the animation for profile pic and info on every refresh
-    triggerSlideInAnimation();
-    profileInfo.classList.add('info-slide-in');
-
-    // Additional code if needed
-    window.onload = function () {
-        // Additional code if needed
-    };
-
-    const profilePic = document.getElementById('profile-pic');
-    const profileInfo = document.querySelector('.profile-info');
-
-    if (profilePic && profileInfo) {
-        profilePic.addEventListener('click', function () {
-            profileInfo.classList.toggle('active');
-        });
-    }
-});
-
 // Function to trigger slide-in animation
 function triggerSlideInAnimation() {
     const profilePic = document.getElementById('profile-pic');
-
-    // Check if the profilePic element exists
-    if (profilePic) {
-        profilePic.classList.add('slide-in');
-    } else {
-        console.error("Element with id 'profile-pic' not found.");
-    }
+    profilePic.classList.add('slide-in');
 }
 
-// Function to toggle theme
 function toggleTheme() {
     document.body.classList.toggle('dark-theme');
     // Save the theme preference to local storage
@@ -66,7 +14,6 @@ function toggleTheme() {
     adjustImageStyleWithAnimation();
 }
 
-// Function to adjust image style
 function adjustImageStyle() {
     const profilePic = document.getElementById('profile-pic');
 
@@ -78,7 +25,6 @@ function adjustImageStyle() {
     }
 }
 
-// Function to adjust image style with animation
 function adjustImageStyleWithAnimation() {
     const profilePic = document.getElementById('profile-pic');
 
@@ -94,16 +40,55 @@ function adjustImageStyleWithAnimation() {
     }, 500); // Adjust the duration to match the CSS transition duration
 }
 
-// Function to show/hide content by ID
+// Function to submit form
+function submitForm(e) {
+    e.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    var formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("message", message);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://formspree.io/f/mleykvjy", true); // Update with your Formspree endpoint
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200 || xhr.status == 0) { // Status 0 is for local testing
+                // Handle the response, you can show a success message or redirect the user
+                if (xhr.responseText === "success") {
+                    console.log("Form submitted successfully!");
+                    // You can add a success message or redirect the user to a thank you page
+                } else {
+                    console.log("Error submitting form!");
+                    // Handle the error, show an error message or redirect the user to an error page
+                }
+            } else {
+                console.log("Error: " + xhr.status);
+                // Handle other HTTP status codes if needed
+            }
+        }
+    };
+
+    xhr.send(formData);
+}
+
 function showContent(id) {
+    // Get the element by ID
     var contentElement = document.getElementById(id);
 
+    // Toggle the 'hidden' class to show/hide the content
     if (contentElement) {
         contentElement.style.display = (contentElement.style.display === 'none' || contentElement.style.display === '') ? 'block' : 'none';
     } else {
         console.error("Element not found with ID:", id);
     }
 }
+
 
 // Function to submit the form
 function submitForm() {
